6e3d0af1bd6bb81f4e410019e071487a80cc2487
bytethenoodle <elbertjohnalejoorozco@gmail.com>
2017-08-02 02:40:52 +0800

    Switched to forked random

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "Crypto",
     dependencies: [
         // Module for generating random bytes and numbers.
-        .Package(url: "https://github.com/vapor/random.git", majorVersion: 1),
+        .Package(url: "https://github.com/bytethenoodle/random.git", majorVersion: 1),
 
         // LibreSSL / OpenSSL module map for Swift.
         .Package(url: "https://github.com/vapor/ctls.git", majorVersion: 1),
