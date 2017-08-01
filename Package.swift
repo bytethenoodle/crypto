4738d66d6e602fc80dd1b6071132fa5f84839645
bytethenoodle <elbertjohnalejoorozco@gmail.com>
2017-08-02 03:55:05 +0800

    mov

--- Package.swift
+++ Package.swift
@@ -4,9 +4,9 @@ let package = Package(
     name: "Crypto",
     dependencies: [
         // Module for generating random bytes and numbers.
-        .Package(url: "https://github.com/bytethenoodle/random.git", majorVersion: 1),
+        .Package(url: "https://github.com/bytethenoodle/random.git", majorVersion: 1)//,
 
         // LibreSSL / OpenSSL module map for Swift.
-        .Package(url: "https://github.com/vapor/ctls.git", majorVersion: 1),
+        //.Package(url: "https://github.com/vapor/ctls.git", majorVersion: 1),
     ]
 )
